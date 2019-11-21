ab0d1ebf1815721e2bb822cc6ac478d9aa69f4f9
Sid Kandasamy <sidkandasamy@outlook.com>
2019-11-21 13:06:32 +0000

    slight changes to output

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ function getHistory(){
 }
 
 function printHistory(num){
-    document.getElementById("history-value").innertext=num; //this function is used to get whatever the history calculations value
+    document.getElementById("history-value").innerText=num; //this function is used to get whatever the history calculations value
+}
+
+// outputs 
+
+function printOutput(num){
+    document.getElementById("output-value").innerText=num; //gets the output value
+}
+
+printOutput("100")
+
+function getOutput(num){
+    document.getElementById("output-value").innerText=num;
 }
 
