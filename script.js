246cc26a7b7064c1c8526be736335d35152851fc
Sid Kandasamy <sidkandasamy@outlook.com>
2019-11-21 12:54:29 +0000

    functions for retriving history values completed

---
+++ script.js
@@ -0,0 +1,8 @@
+function getHistory(){
+    return document.getElementById("history-value").innerText; //get the value of the history (calulation e.g 10+10) by using inner text
+}
+
+function printHistory(num){
+    document.getElementById("history-value").innertext=num; //this function is used to get whatever the history calculations value
+}
+
