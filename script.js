d84d7b2ec950b9c43bd0fe240206842ea6329d56
Sid Kandasamy <sidkandasamy@outlook.com>
2019-11-28 11:27:18 +0000

    calculation history works, however result does not

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 function getHistory(){
-    return document.getElementById("history-value").innerText; //get the value of the history (calulation e.g 10+10) by using inner text
+	return document.getElementById("history-value").innerText; //get the value of the history (calulation e.g 10+10) by using inner text
 }
-
 function printHistory(num){
-    document.getElementById("history-value").innerText=num; //this function is used to get whatever the history calculations value
+	document.getElementById("history-value").innerText=num; //this function is used to get whatever the history calculations value
 }
-
-// outputs 
-
-function getOutput(num){
-    document.getElementById("output-value").innerText=num;
+function getOutput(){
+	return document.getElementById("output-value").innerText;
 }
-
-
-
 function printOutput(num){
-  
-    if(num==""){ //if valiue is empty set the output ot empty
+	if(num==""){ //if valiue is empty set the output ot empty
 		document.getElementById("output-value").innerText=num;//gets the output value
 	}
 	else{ //or else convert it to formatted number
 		document.getElementById("output-value").innerText=getFormattedNumber(num);
 	}	
 }
-
-
-
-function getFormattedNumber(num){ //this function reads the value and formats the number output
+function getFormattedNumber(num){
 	if(num=="-"){
 		return "";
 	}
@@ -34,10 +23,6 @@ function getFormattedNumber(num){ //this function reads the value and formats th
 	var value = n.toLocaleString("en");
 	return value;
 }
-
-
-
-
 function reverseNumberFormat(num){
 	return Number(num.replace(/,/g,''));
 }
@@ -56,6 +41,29 @@ for (let i = 0; i < operator.length; i++) {
             printHistory("");
             printOutput("");
         }
+        else{
+			var output=getOutput();
+			var history=getHistory();
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substr(0,history.length-1);
+				}
+			}
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
+		}
 
     })
     
@@ -66,23 +74,25 @@ for (let i = 0; i < operator.length; i++) {
 //numbers
 
 let numbers = document.getElementsByClassName("number")
-
-for (let i = 0; i < numbers.length; i++) {
     
-    numbers[i].addEventListener('click',function(){
-        // alert("this works"+this.id);
-        //get output without commers removed
-        var output = reverseNumberFormat(getOutput());
-        
-        if (output!=Nan){ //if output is a number
-            output=output+this.id;
-            printOutput(output);
-        }
-    
-      
+    for(var i =0;i<numbers.length;i++){
+        //alert(:this works"+id);
+        //getoutput without commas removed
+        numbers[i].addEventListener('click',function(){
+            var output=reverseNumberFormat(getOutput());
+            if(output!=NaN){ //if output is a number
+                output=output+this.id;
+                printOutput(output);
+            }
+        });
 
-    });
     
 }
 
+
+
+
 //use for loop to access each numbers one by one and add click function
+
+
+
